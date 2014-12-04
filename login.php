0bb7fc13ea8bd5bed00b399662aa992e4496bbc0
Chris <me@me.com>
2014-12-04 16:50:05 -0600

    Inital commit

---
+++ login.php
@@ -0,0 +1,104 @@
+<?php 
+    require("config.php"); 
+    $submitted_username = ''; 
+    if(!empty($_POST)){ 
+        $query = " 
+            SELECT 
+                id, 
+                username, 
+                password, 
+                salt, 
+                email 
+            FROM users 
+            WHERE 
+                username = :username 
+        "; 
+        $query_params = array( 
+            ':username' => $_POST['username'] 
+        ); 
+         
+        try{ 
+            $stmt = $db->prepare($query); 
+            $result = $stmt->execute($query_params); 
+        } 
+        catch(PDOException $ex){ die("Failed to run query: " . $ex->getMessage()); } 
+        $login_ok = false; 
+        $row = $stmt->fetch(); 
+        if($row){ 
+            $check_password = hash('sha256', $_POST['password'] . $row['salt']); 
+            for($round = 0; $round < 65536; $round++){
+                $check_password = hash('sha256', $check_password . $row['salt']);
+            } 
+            if($check_password === $row['password']){
+                $login_ok = true;
+            } 
+        } 
+
+        if($login_ok){ 
+            unset($row['salt']); 
+            unset($row['password']); 
+            $_SESSION['user'] = $row;  
+            header("Location: dashboard.php"); 
+            die("Redirecting to: dashboard.php"); 
+        } 
+        else{ 
+            echo "<div class='container hero-unit'><h3>Wrong Password</h3></div>"; 
+            $submitted_username = htmlentities($_POST['username'], ENT_QUOTES, 'UTF-8'); 
+        } 
+    } 
+?> 
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Main Page</title>
+    <meta name="description" content="Login Page">
+    <meta name="dsu" content="dsu.edu">
+
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+    <script src="assets/bootstrap.min.js"></script>
+    <link href="assets/bootstrap.min.css" rel="stylesheet" media="screen">
+    <style type="text/css">
+        body  
+        .hero-unit { background-color: #fff; }
+        .center { display: block; margin: 0 auto; }
+    </style>
+</head>
+
+<body>
+
+<div class="navbar navbar-fixed-top navbar-inverse">
+  <div class="navbar-inner">
+    <div class="container">
+      <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </a>
+      <a class="brand">DSU Spearphishing</a>
+      <div class="nav-collapse collapse">
+        <ul class="nav pull-right">
+          <li><a href="register.php">Register</a></li>
+          <li class="divider-vertical"></li>
+          <li class="dropdown">
+            <a class="dropdown-toggle" href="#" data-toggle="dropdown">Log In <strong class="caret"></strong></a>
+            <div class="dropdown-menu" style="padding: 15px; padding-bottom: 0px;">
+                <form action="login.php" method="post"> 
+                    Username:<br /> 
+                    <input type="text" name="username" value="<?php echo $submitted_username; ?>" /> 
+                    <br /><br /> 
+                    Password:<br /> 
+                    <input type="password" name="password" value="" /> 
+                    <br /><br /> 
+                    <input type="submit" class="btn btn-info" value="Login" /> 
+                </form> 
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+</body>
+</html>
